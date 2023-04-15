0f9dfd8e8891314a8f55307cc1771be9eb72083d
Maz <mazspm@icloud.com>
2023-04-15 15:49:02 -0500

    view single post

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,19 +1,49 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { PageLayout } from "~/components/PageLayout";
+import { PostView } from "~/components/PostView";
+import { generateSSGHelper } from "~/server/utils/ssgHelper";
+import { api } from "~/utils/api";
 
-const SinglePostPage: NextPage = () => {
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+  const id = context.params?.id;
+
+  if (typeof id !== "string") throw new Error("Post id is not a string");
+
+  await ssg.posts.getPostById.prefetch({ id: id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+    revalidate: 1, // revalidate every 1 second
+  };
+};
+
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getPostById.useQuery({
+    id: id,
+  });
+
+  if (!data) return <div>404</div>;
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>
-          <div>single post view</div>
-        </div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
+};
+
+// This function generates all possible paths on the server
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default SinglePostPage;
